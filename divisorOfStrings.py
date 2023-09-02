8893e812a5f9d83dc2ea94e5f2e37daf5ed3ce32
zahrabateninia <zahrabatenin@gmail.com>
2023-09-02 17:30:41 +0330

    recursive function explanation

--- divisorOfStrings.py
+++ divisorOfStrings.py
@@ -12,9 +12,16 @@ def gcdOfStrings(str1, str2):
         if len(str1) == len(str2):
             return str1
         if len(str1) > len(str2):
-            return gcdOfStrings(str1[len(str2):], str2)
+            return gcdOfStrings(str1[len(str2):], str2) # using recursive function
         return gcdOfStrings(str1, str2[len(str1):])
 
 
 print(gcdOfStrings(str1,str2))
+"""
+If str1 is longer than str2, the function recursively calls itself with the substring of str1 
+that starts at the length of str2 and the original str2. 
+This effectively removes the common prefix of str2 from str1 and 
+continues the search for a common divisor in the remaining part of str1.
+
+"""
 
