36de1900661425f1c06b9383c7679ab53a957c7b
zahrabateninia <zahrabatenin@gmail.com>
2023-09-04 18:31:55 +0330

    checks if there is enough place for desired number of flowrs

---
+++ canPlaceFlowers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+You have a long flowerbed in which some of the plots are planted, 
+and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's,
+where 0 means empty and 1 means not empty, and an integer n,
+return true if n new flowers can be planted in the flowerbed 
+without violating the no-adjacent-flowers rule and false otherwise.
+
+"""
+flowerbed = [1,0,1,0,1]
+numOfFlowers = 1
+numOfPlantedFlowers = 0
+
+def canPlantFlowers(flowerbed):
+    i = 0
+    global numOfPlantedFlowers
+    for flower in flowerbed:
+        if flower == 0 and (flowerbed[i + 1] == 0)  and (flowerbed[i - 1] == 0): 
+            numOfPlantedFlowers += 1
+        i += 1
+    if numOfPlantedFlowers < numOfFlowers:
+        print(False)
+    else:
+        print(True)
+
+canPlantFlowers(flowerbed)
+
