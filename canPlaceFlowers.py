2e75c246c01bdaf4afda3a5a6e52f44331f74987
zahrabateninia <zahrabatenin@gmail.com>
2023-09-05 19:35:15 +0330

    passed all the testcases and edgecases

--- canPlaceFlowers.py
+++ canPlaceFlowers.py
@@ -12,19 +12,17 @@ without violating the no-adjacent-flowers rule and false otherwise.
 
 def canPlaceFlowers(flowerbed: List[int], n: int) -> bool:
     numOfPlantedFlowers = 0
-
     i = 0
-    numOfPlantedFlowers = 0
-    for flower in flowerbed:
-        if flower == 0 and (flowerbed[i + 1] == 0) and (flowerbed[i - 1] == 0): 
+    while i < len(flowerbed):
+        if (
+            flowerbed[i] == 0 and
+            (i == 0 or flowerbed[i - 1] == 0) and
+            (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)
+        ):
             flowerbed[i] = 1
             numOfPlantedFlowers += 1
         i += 1
-    if numOfPlantedFlowers < n:
-        return(False)
-    else:
-        return(True)
 
-
+    return numOfPlantedFlowers >= n
 
         
