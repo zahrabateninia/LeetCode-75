692e70f8180e186c79fa734e460fcd893d6de302
zahrabateninia <zahrabatenin@gmail.com>
2023-09-06 15:13:15 +0330

    10 testcases passed

--- reverseVowels.py
+++ reverseVowels.py
@@ -3,19 +3,29 @@ Given a string s, reverse only all the vowels in the string and return it.
 The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both 
 lower and upper cases, more than once.
 """
-s = "hello"
+s = "leetcOde"
+markedVowels = ''
 vowels = ['a','e','o','u','i']
 newS = ''
 vowelsOfS= []
+i = 0
 
-for letter in s:
+for letter in s.lower():
     for vowel in vowels:
         if vowel == letter:
             vowelsOfS.append(letter)
+            letter = '0'
+    markedVowels += letter
 # imp: The reverse() method doesn't return any value. It updates the existing list.
-
 reverseVowels = vowelsOfS.copy()
 reverseVowels.reverse()
-print(reverseVowels)
+
+for letter in markedVowels:
+    if letter == '0':
+        letter = reverseVowels[i]
+        i +=1
+    newS += letter
+print(newS)
+
 
 
