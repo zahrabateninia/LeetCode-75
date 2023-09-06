64043baa4d9187d002fe7b1c4370ad4dcc60bf8a
zahrabateninia <zahrabatenin@gmail.com>
2023-09-06 16:22:15 +0330

    another solution

--- reverseVowels.py
+++ reverseVowels.py
@@ -3,28 +3,19 @@ Given a string s, reverse only all the vowels in the string and return it.
 The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both 
 lower and upper cases, more than once.
 """
-
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        markedVowels = ''
-        newS = ''
-        vowelsOfS= []
-        i = 0
-
-        for letter in s:
-            if letter == 'a' or letter == 'o' or letter == 'e' or letter == 'u' or letter == 'i' or letter == 'A'or letter == 'E'or letter == 'U'or letter == 'I'or letter == 'O':
-                
-                    vowelsOfS.append(letter)
-                    letter = '|'
-            markedVowels += letter
-        # imp: The reverse() method doesn't return any value. It updates the existing list.
-        reverseVowels = vowelsOfS.copy()
-        reverseVowels.reverse()
-
-        for letter in markedVowels:
-            if letter == '|':
-                letter = reverseVowels[i]
-                i +=1
-            newS += letter
-        return(newS)
-
+        s = list(s)
+        vowels = {'a', 'o', 'i', 'e', 'u', 'A', 'O', 'I', 'E', 'U'}
+        l, r = 0, len(s)-1
+        while l < r:
+            if s[l] in vowels and s[r] in vowels:
+                s[l], s[r] = s[r], s[l]
+                l += 1
+                r -= 1
+            else:
+                if s[l] not in vowels:
+                    l += 1
+                if s[r] not in vowels:
+                    r -= 1
+        return ''.join(s)
