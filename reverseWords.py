ddf6c4e959afc51d469e69518f0f5853ac61dfa3
zahrabateninia <zahrabatenin@gmail.com>
2023-09-07 23:35:05 +0330

    deleting spaces in the given str

---
+++ reverseWords.py
@@ -0,0 +1,22 @@
+"""
+Given an input string s, reverse the order of the words.
+A word is defined as a sequence of non-space characters. 
+The words in s will be separated by at least one space.
+Return a string of the words in reverse order concatenated by a single space.
+
+"""
+import re
+
+s = "the sky is   blue"
+words = re.findall(r"\w*", s)  # ['the', '', 'sky', '', 'is', '', '', '', 'blue', '']
+noSpaceWords = []
+for word in words:
+    if word == "":
+        continue
+    noSpaceWords.append(word)
+
+print(noSpaceWords)
+
+# reversedL = listOfS.copy()
+# reversedL = reversedL.reverse()
+# print(reversedL)
