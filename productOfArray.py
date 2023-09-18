f08eacf21c52a337a9a234770bc8c4f0f9545fb0
zahrabateninia <zahrabatenin@gmail.com>
2023-09-18 12:10:21 +0330

    all testcases passed with a great runtime

--- productOfArray.py
+++ productOfArray.py
@@ -4,18 +4,17 @@ the product of all the elements of nums except nums[i].
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 You must write an algorithm that runs in O(n) time and without using the division operation.
 '''
+# result[i] = multiply of all left * multiply of all right
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        products = [1] * length
+        for i in range(1, length):
+            products[i] = products[i-1] * nums[i-1]
 
-nums = [1,2,3,4]
-answer=[] 
-i = 0
-
-while i < len(nums): #i =0
-    popped_item = nums.pop(i) # popped_item = 1
-    result_of_multiply = 1
-    for num in nums: # num = 2 first num 
-        result_of_multiply *= num # 2 then 6 and then 24 so result is 24 
-
-    answer.append(result_of_multiply)# answer = [24]
-    nums.insert(i, popped_item)  # nums is again the same as before the while loop
-    i += 1
-print(answer)
+        right = nums[-1]
+        for i in range(length-2, -1, -1):
+            products[i] *= right
+            right *= nums[i]
+        
+        return products
