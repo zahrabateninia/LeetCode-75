dc4c60eb0cc02855d0de94655775e20825b78b02
zahrabateninia <zahrabatenin@gmail.com>
2023-09-23 12:53:02 +0330

    solved using swap

--- moveZeros.py
+++ moveZeros.py
@@ -3,4 +3,10 @@
 # Note that you must do this in-place without making a copy of the array.
 
 def moveZeroes(nums: List[int]) -> None:
-            nums.sort(key=lambda x: x == 0)
+            # nums.sort(key=lambda x: x == 0) another solution 
+            non_zero_idx = 0
+
+            for i in range(len(nums)):
+                if nums[i] != 0:
+                    nums[i], nums[non_zero_idx] = nums[non_zero_idx], nums[i] # syntax of swap in python: a,b = b,a   :)
+                    non_zero_idx += 1
