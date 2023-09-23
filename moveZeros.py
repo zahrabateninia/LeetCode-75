c0aef79c9e4cce075a647a0ca55523a71ffc14da
zahrabateninia <zahrabatenin@gmail.com>
2023-09-23 12:49:02 +0330

    solved using lambda and sort() functions

--- moveZeros.py
+++ moveZeros.py
@@ -2,13 +2,5 @@
 
 # Note that you must do this in-place without making a copy of the array.
 
-nums = [0,3,12,0,3] # [3,12,3,0,0]
-zeros=[]
-
-for num in nums:
-    if num == 0:
-        zeros.append(num)
-        nums.remove(num)
-
-nums.extend(zeros)
-print(nums)
+def moveZeroes(nums: List[int]) -> None:
+            nums.sort(key=lambda x: x == 0)
