2720b4fd190472732e6b48df43a712b694d5e1db
zahrabateninia <zahrabatenin@gmail.com>
2023-10-09 18:14:39 +0330

    covert binary num to decimal

--- concatenateBinary.py
+++ concatenateBinary.py
@@ -13,3 +13,16 @@ remainders.reverse()
 strRemainders = ''.join(remainders) # convert the list to string
 
 print(strRemainders)
+num = 1001011
+listNum = [int(x) for x in str(num)]
+sqr = 0
+decimal = 0
+listNum.reverse()
+for digit in listNum:
+    if digit == 1:
+        digit = 2**sqr
+        decimal += digit
+    
+    sqr += 1
+print(decimal)
+        
