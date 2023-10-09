d00c4f6ae391920e8f9cbc946cf6a855fd4b24cd
zahrabateninia <zahrabatenin@gmail.com>
2023-10-09 18:37:45 +0330

    time limit exceeded error, passed half of the testcases

--- concatenateBinary.py
+++ concatenateBinary.py
@@ -1,28 +1,22 @@
 """
 Given an integer n, return the decimal value of the binary string formed by 
 concatenating the binary representations of 1 to n in order, modulo 109 + 7.
-"""
-n=75
-remainders = []
-while n > 0:
-    d = n//2
-    r = n % 2
-    remainders.append(str(r))
-    n = d
-remainders.reverse()
-strRemainders = ''.join(remainders) # convert the list to string
+# """
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+            concatenated_binary = ''
 
-print(strRemainders)
-num = 1001011
-listNum = [int(x) for x in str(num)]
-sqr = 0
-decimal = 0
-listNum.reverse()
-for digit in listNum:
-    if digit == 1:
-        digit = 2**sqr
-        decimal += digit
-    
-    sqr += 1
-print(decimal)
-        
+            for i in range(1, n+1):
+                # Convert i to binary and append to concatenated_binary
+                binary_i = bin(i)[2:]  # Removing the '0b' prefix
+                concatenated_binary += binary_i
+
+            # Convert binary to decimal
+            decimal_value = 0
+            for idx, bit in enumerate(reversed(concatenated_binary)):
+                if bit == '1':
+                    decimal_value += 2 ** idx
+
+            # Calculate modulo 10^9 + 7
+            result = decimal_value % (10**9 + 7)
+            return result
