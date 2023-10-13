13318a5563e236936988356d703aafdb8c2bd5d1
zahrabateninia <zahrabatenin@gmail.com>
2023-10-13 06:02:57 +0330

    solved by window sliding method

--- findMaxAverage.py
+++ findMaxAverage.py
@@ -4,25 +4,20 @@ Find a contiguous subarray whose length is equal to k that has the maximum avera
 and return this value. Any answer with a calculation error less than 10-5 will be accepted.
 """
 
-
 def findMaxAverage(nums, k: int) -> float:
-            
-    start_idx = 0
-    end_idx = k-1
-    max_sum = 0
-    length = len(nums)
-    window_sum = 0
-    # first window
-    for i in range(start_idx, end_idx+1):
-        window_sum += i 
-    num_of_window = length - k
-    
-    if max_sum < window_sum:
-        max_sum = window_sum
-    return max_sum/ k
 
+        start_idx = 0
+        end_idx = k-1
+        max_sum = float('-inf')  # Initialize max_sum with negative infinity
+        length = len(nums)
+        window_sum = sum(nums[start_idx:end_idx+1])  # Calculate sum of first window
 
+        while end_idx < length - 1:
+            max_sum = max(max_sum, window_sum)  # Update max_sum if necessary
+            window_sum += nums[end_idx+1] - nums[start_idx]  # Update window_sum
+            start_idx += 1
+            end_idx += 1
 
-nums = [1,12,-5,-6,50,3]
-k = 4
-print(findMaxAverage(nums,k))
+        max_sum = max(max_sum, window_sum)  # Check max_sum one last time
+        return max_sum / k
+
