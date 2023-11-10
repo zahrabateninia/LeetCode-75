0dc51c4b20dd1fe815e6a64f284d73c7f5eddb89
zahrabateninia <zahrabatenin@gmail.com>
2023-11-10 13:25:19 +0330

    Add description of the problem

---
+++ longestSubarray.py
@@ -0,0 +1,9 @@
+"""
+Given a binary array nums, you should delete one element from it.
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. 
+Return 0 if there is no such subarray.
+"""
+
+
+def longestSubarray(self, nums: List[int]) -> int:
+            
