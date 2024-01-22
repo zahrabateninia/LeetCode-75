eaef1d082d313eeafe8c494d00d67c9859dfd6f2
zahrabateninia <zahrabatenin@gmail.com>
2024-01-22 13:50:22 +0330

    Optimize the solution by creating a dict directly form the arr, Time Complexity: O(n)

--- hashMap/uniqueOccurrences.py
+++ hashMap/uniqueOccurrences.py
@@ -1,18 +1,14 @@
 # Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
 
+
 def uniqueOccurrences(arr) -> bool:
-    # Convert arr to a set
-    uniqueMembers = set(arr)
-    uniqueOccur = {}
+    # Create a frequency dict in a single pass
+    freqDict = {}
+    for num in arr:
+        if num in freqDict:
+            freqDict[num] += 1
+        else:
+            freqDict[num] = 1
 
-    for member in uniqueMembers:
-        memberFreq = arr.count(member)
-        # Create a frequency dict
-        uniqueOccur[member] = memberFreq
-    return (len(set(uniqueOccur.values())) == len(uniqueMembers))
-
-
-
-
-# arr = [1,2,2,1,1,3]
-# uniqueMembers = {1,2,3}
+    # Check if the occurrences are unique
+    return len(set(freqDict.values())) == len(freqDict.values())
