51a5be2e6d1b65ad8113664e4532a647ef291987
zahrabateninia <zahrabatenin@gmail.com>
2024-02-01 11:59:42 +0330

    Import the collections module in order to use Counter class in the code

--- hashMap/closeStrings.py
+++ hashMap/closeStrings.py
@@ -1,35 +1,6 @@
-# Two strings are considered close if you can attain one from the other using the following operations:
+from collections import Counter
 
-# Operation 1: Swap any two existing characters.
-# For example, abcde -> aecdb
-# Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
-# For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
-# You can use the operations on either string as many times as necessary.
-
-# Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
-
-    
-# def countOccurrenceOfLetters(word):
-#     occurrenceOfLetters = {}
-#     for letter in word:
-#         if letter in occurrenceOfLetters:
-#             occurrenceOfLetters[letter] += 1
-#         else:
-#             occurrenceOfLetters[letter] = 1
-#     return occurrenceOfLetters
-
-# def closeStrings(word1: str, word2: str) -> bool:
-#     # if their lengths aren't the same return false
-#     if len(word1) != len(word2):
-#          return False
-
-#     freqWord1Dict = countOccurrenceOfLetters(word1)
-#     freqWord2Dict = countOccurrenceOfLetters(word2)
-
-#     return set(freqWord1Dict) == set(freqWord2Dict) and sorted(freqWord1Dict.values()) == sorted(freqWord2Dict.values())
-
-  
-
+# The Counter class is used in your code to count the occurrences of characters in the given words
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         return len(word1) == len(word2) \
