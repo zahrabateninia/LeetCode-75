8efd18927d0419d308df78557db10f08cdfe80c2
zahrabateninia <zahrabatenin@gmail.com>
2024-02-01 10:27:36 +0330

    Count the occurrence of letters in the given words and save them in a dictionary

---
+++ hashMap/closeStrings.py
@@ -0,0 +1,32 @@
+# Two strings are considered close if you can attain one from the other using the following operations:
+
+# Operation 1: Swap any two existing characters.
+# For example, abcde -> aecdb
+# Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+# For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+# You can use the operations on either string as many times as necessary.
+
+# Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+
+    
+def countOccurrenceOfLetters(word):
+    occurrenceOfLetters = {}
+    for letter in word:
+        if letter in occurrenceOfLetters:
+            occurrenceOfLetters[letter] += 1
+        else:
+            occurrenceOfLetters[letter] = 1
+    return occurrenceOfLetters
+
+def closeStrings(word1: str, word2: str) -> bool:
+    # if their lengths aren't the same return false
+    if len(word1 != word2): return False
+
+    freqWord1Dict = countOccurrenceOfLetters(word1)
+    freqWord2Dict = countOccurrenceOfLetters(word2)
+
+    # compare the values of these two dictionaries 
+    
+
+
+
