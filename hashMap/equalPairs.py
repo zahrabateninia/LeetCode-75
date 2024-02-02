6afc291e49fcaa2d73b55825d21672e67256a0b4
zahrabateninia <zahrabatenin@gmail.com>
2024-02-02 23:24:11 +0330

    Add the description with an example

---
+++ hashMap/equalPairs.py
@@ -0,0 +1,8 @@
+# Equal Row and Column Pairs
+
+# Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
+# A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
+
+grid = [[3,2,1],[1,7,6],[2,7,7]]
+# Output should be 1
+def equalPairs(grid) -> int:
