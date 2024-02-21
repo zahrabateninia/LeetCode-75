1a163bc08dbbab089a92952c4951d28a0bc67f61
zahrabateninia <zahrabatenin@gmail.com>
2024-02-21 23:17:14 +0330

    Improve time complexity using Hash Maps instead of nested loops

--- hashMap/equalPairs.py
+++ hashMap/equalPairs.py
@@ -6,3 +6,7 @@
 grid = [[3,2,1],[1,7,6],[2,7,7]]
 # Output should be 1
 def equalPairs(grid) -> int:
+
+
+# I have already the rows as arrays in grid array
+# what I need: columns
--- twoSum.js
+++ twoSum.js
@@ -6,15 +6,22 @@
 
 const unsortedNrs = [4,2,6,3,1,5,9,7,8,10]
 
-const twoSumBrute = (list, target) => {
+const twoSumHashmap = (list, target) => {
+  const lookupTable = {}
+  
+  // build a lookup table
   for (let i = 0; i < list.length; i++) {
-    for (let j = 0; j < list.length; j++) {
-      if (list[i] + list[j] === target) {
-          return [i, j]
-      }
+    lookupTable[list[i]] = i;
+  }
+  
+  // iterate
+  for (let j = 0; j < list.length; j++) {
+    let diff = target - list[j]
+    if (lookupTable[diff] && lookupTable[diff] !== j) {
+      return [j, lookupTable[diff]]
     }
   }
 }
 
-twoSumBrute(unsortedNrs, 7)
+twoSumHashmap(unsortedNrs, 7)
 // OUTPUT => [0, 3]
