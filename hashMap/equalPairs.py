71d39a3f6f8ac00d8eaf953fdc9427f7b31f80bf
zahrabateninia <zahrabatenin@gmail.com>
2024-04-17 11:59:48 +0330

    Convert each row of the grid to tuple and create a rows dict and set those tuples as keys and their values are set as 1( if already exists increment it)

--- hashMap/equalPairs.py
+++ hashMap/equalPairs.py
@@ -8,4 +8,19 @@
 grid = [[3,2,1],[1,7,6],[2,7,7]]
 # Output should be 1
 def equalPairs(grid) -> int:
-    
+    numberOfRows = len(grid)
+    rows = {}
+    count = 0
+    for r in range(numberOfRows): # for every row in the grid
+        # row is the key of rows dictionary
+        row = tuple(grid[r]) # so [3,2,1] (our first row) is converted to (3,2,1)
+        # set the value of each key in rows dict
+        rows[row] = 1 + rows.get(row, 0) 
+
+
+
+
+
+# My Note: 
+# The get() method returns the value of the item with the specified key.
+
