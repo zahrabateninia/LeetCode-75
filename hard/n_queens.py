c94b003992eb882e5eaa2f364bc1545ceca3a3ef
zahrabateninia <zahrabatenin@gmail.com>
2024-11-24 19:10:12 +0330

    Use backtracking to solve n-queens problem

--- hard/n_queens.py
+++ hard/n_queens.py
@@ -21,3 +21,29 @@ def solveNQueens(n):
         # copy each row in board and join each row together
         copy = ["".join(row) for row in board]
         res.append(copy)
+        return
+
+        # if we did not reach the base case
+
+        # we are not allowed to use this column and row condition:
+        for c in range(n):
+            if c in col or(c+r) in posDiag or (r-c) in negDiag:
+                continue
+            # update
+            col.add(c)
+            posDiag.add(c+r)
+            negDiag.add(r-c)
+            board[r][c]= "Q" # place the queen not . anymore
+
+
+            # go to the next row
+            backtrack(r+1)
+
+            # undo the things, that's what backtracking means
+            col.remove(c)
+            posDiag.remove(c+r)
+            negDiag.remove(r-c)
+            board[r][c]= "." 
+            
+        backtrack(0)
+        return res
