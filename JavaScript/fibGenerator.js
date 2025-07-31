ad285cf3ba3ee45d29ff30bef800431289e730ec
zahrabateninia <zahrabatenin@gmail.com>
2025-07-31 14:09:16 +0330

    Define generateFibSequence to call the generator callCount times

--- JavaScript/fibGenerator.js
+++ JavaScript/fibGenerator.js
@@ -7,7 +7,7 @@
 /**
  * @return {Generator<number>}
  */
-var fibGenerator = function*() {
+const fibGenerator = function*() {
 
   let current = 0; 
   let next = 1;
@@ -20,8 +20,19 @@ var fibGenerator = function*() {
   }
 };
 
-/**
- * const gen = fibGenerator();
- * gen.next().value; // 0
- * gen.next().value; // 1
- */
+ const gen = fibGenerator();
+ console.log(gen.next().value); // 0
+ console.log(gen.next().done); // false
+ console.log(gen.next().value); // 1
+ 
+ function generateFibSequence(callCount){
+    const gen = fibGenerator()
+    result = []
+    for(i=0; i<= callCount; i++){
+      result.push(gen.next().value)
+    }
+    return result
+ }
+
+ const fibSeq = generateFibSequence(5)
+ console.log(fibSeq)
