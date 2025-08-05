2685762f4987130dc13d8b211c9427aad890681f
zahrabateninia <zahrabatenin@gmail.com>
2025-08-05 19:33:06 +0330

    Implement custom filter function with index support

---
+++ JavaScript/filter.js
@@ -0,0 +1,17 @@
+ /**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+
+ const filter = function(arr, fn) {
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {  
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+};
